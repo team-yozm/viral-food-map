d14f060c2fce211d754dd654a40834db8c4bc04f
firstghrn <firstghrn@naver.com>
2026-03-26 00:43:53 +0900

    Add dev auto-deploy setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   async headers() {
     return [
       {
